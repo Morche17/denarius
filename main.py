50382677048290b55f8e490de6fed92ac3ba8a4e
Emanuel Tavares <103008206+Morche17@users.noreply.github.com>
2022-12-16 20:52:42 +0000

    Adición del comando "¡ping" y mensaje por terminal cuando el bot se inicialice ('\nMy bot is ready :)'). Se cambió el comando "$hello" por "¡hola"

--- main.py
+++ main.py
@@ -7,17 +7,22 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
+    
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    print('\nMy bot is ready :)')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('¡hola'):
+        await message.channel.send('¡Saludos!')
+
+    if message.content.startswith('¡ping'):
+        await message.channel.send('Pong')
+
 
 client.run(my_secret)
